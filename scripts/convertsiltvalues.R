4cc43f44ea4ec89b88cb554921cbb88ac9179703
fernstgruber <fabianernstgruber@hotmail.com>
2017-12-04 07:41:55 +0100

    bisserl was bei thalheimerprofile

--- scripts/convertsiltvalues.R
+++ scripts/convertsiltvalues.R
@@ -31,4 +31,67 @@ textur <- schluff_oe(portions = portions,breaks=breaks)
 textur
 textur_oe(t=textur[1],u=textur[2])
 #######################################################
-
+#######################################################
+#######################################################
+#######################################################
+##PROFIL 2 von Martin Thalheimer
+##  Horizont Ap1
+portions <- c(11,43,10,36)
+breaks <- c(0.002,0.05,0.1,2)
+textur <- schluff_oe(portions = portions,breaks=breaks)
+textur
+textur_oe(t=textur[1],u=textur[2])
+#######################################################
+##  Horizont Ap2
+portions <- c(10,44,10,36)
+breaks <- c(0.002,0.05,0.1,2)
+textur <- schluff_oe(portions = portions,breaks=breaks)
+textur
+textur_oe(t=textur[1],u=textur[2])
+#######################################################
+##  Horizont C
+portions <- c(5,29,11,55)
+breaks <- c(0.002,0.05,0.1,2)
+textur <- schluff_oe(portions = portions,breaks=breaks)
+textur
+textur_oe(t=textur[1],u=textur[2])
+#######################################################
+##  Horizont Ab
+portions <- c(13,49,12,26)
+breaks <- c(0.002,0.05,0.1,2)
+textur <- schluff_oe(portions = portions,breaks=breaks)
+textur
+textur_oe(t=textur[1],u=textur[2])
+#######################################################
+#######################################################
+#######################################################
+#######################################################
+##PROFIL X von Martin Thalheimer
+##  Horizont 
+portions <- c()
+breaks <- c(0.002,0.05,0.1,2)
+textur <- schluff_oe(portions = portions,breaks=breaks)
+textur
+textur_oe(t=textur[1],u=textur[2])
+#######################################################
+##  Horizont 
+portions <- c()
+breaks <- c(0.002,0.05,0.1,2)
+textur <- schluff_oe(portions = portions,breaks=breaks)
+textur
+textur_oe(t=textur[1],u=textur[2])
+#######################################################
+##  Horizont 
+portions <- c()
+breaks <- c(0.002,0.05,0.1,2)
+textur <- schluff_oe(portions = portions,breaks=breaks)
+textur
+textur_oe(t=textur[1],u=textur[2])
+#######################################################
+##  Horizont 
+portions <- c()
+breaks <- c(0.002,0.05,0.1,2)
+textur <- schluff_oe(portions = portions,breaks=breaks)
+textur
+textur_oe(t=textur[1],u=textur[2])
+#######################################################
