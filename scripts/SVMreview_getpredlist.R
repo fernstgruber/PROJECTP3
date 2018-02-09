7c309173ad35ed1e5e5f937b31c118a8f9d3d8df
fernstgruber <fabianernstgruber@hotmail.com>
2018-02-09 17:40:39 +0100

    neue analysen und ergebnisse

---
+++ scripts/SVMreview_getpredlist.R
@@ -0,0 +1,32 @@
+
+require(e1071)
+require(RCurl)
+require(repmis)
+require(randomForest)
+require(rgdal)
+myfunctions <- getURL("https://raw.githubusercontent.com/fernstgruber/Rstuff/master/fabiansandrossitersfunctions.R", ssl.verifypeer = FALSE)
+eval(parse(text = myfunctions))
+#proj3path="/home/fabs/PROJECTP3"
+proj3path="/media/fabs/Volume/01_PAPERZEUG/PROJECTP3/"
+setwd(proj3path)
+load('./data/dependentlists.RData')
+
+i=1
+fullpredlist <- vector()
+predictorlist <- list()
+for(i in 1:length(dependentlist)){
+  dep=dependentlist[i]
+  print(dependentlist_eng[i])
+  preds <- evaluateforwardCV_anyerror(mypath=paste("./data/FSCV/SVM_geoandgeom/SVMwithgeoandgeom_fw_5fold_10p_",dep,"_allpreds",sep=""),kk=1:5,endround = 10,error = "cverror",geheim = "geheimerprederror",yrange=c(0,1))
+    predictors = vector()
+  for (n in 1:ncol(preds)){
+    for (n2 in 1:nrow(preds)){
+      predictors <- c(predictors,as.character(preds[n2,n]))
+    }
+  } 
+  uniquepredictors <- unique(predictors)
+  predictorlist[[1]] <- uniquepredictors
+  fullpredlist <- c(fullpredlist,uniquepredictors)
+  }
+fullpredlist <- unique(fullpredlist)
+save(fullpredlist,predictorlist,file=("./data/modeldata/predictorlists_SVM.RData"))
