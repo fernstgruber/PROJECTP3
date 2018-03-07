b5885bf06913c238faefc3eaa4d1ab1cbd62d062
fernstgruber <fabianernstgruber@hotmail.com>
2018-03-07 07:47:39 +0100

    gitignore

---
+++ scripts/predictshell.sh
@@ -0,0 +1,6 @@
+
+Rscript predictSF.R 4 "geom_10m_fl10_L6" "geom_10m_fl10_L6"
+Rscript predictSF.R 4 "geom_10m_fl10_L6,slope_ws11" "geom_10m_fl10_L6,slope_ws11_10m"
+Rscript predictSF.R 4 "slope_ws3_hr,minic_DTM_50m_avg_ws3" "slope_ws3_hr_hr,minic_DTM_50m_avg_ws3_50m"
+Rscript predictSF.R 5 "crosc_ws19_hr" "crosc_ws19_hr_hr"
+Rscript predictSF.R 5 "Flow_Line_Curvature_50m" "Flow_Line_Curvature_50m"
